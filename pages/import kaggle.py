a0fb8b12a8230bba97d26881538ffa7c4f18bb13
R-Akira <56804130+R-Akira@users.noreply.github.com>
2020-07-27 17:41:35 -0700

    Delete import kaggle.py

--- pages/import kaggle.py
+++
@@ -1,8 +0,0 @@
-import kaggle
-import os
-
-
-os.chdir("/Users/user/Desktop/MITx")
-kaggle.api.authenticate()
-
-kaggle.api.dataset_download_files('Jan_2019_ontime.csv', path='Jan_2019_ontime.csv', unzip=True)
